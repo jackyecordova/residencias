5b0fb203ccaaa51903aca42e86ee2715f5fd1b3d
Itzelestradaleyva <lezti2294@gmail.com>
2017-10-27 10:03:13 -0600

    se agrego para actualizar devengado y pagado
    :)

---
+++ production/codigos/actualizardevengado.php
@@ -0,0 +1,11 @@
+<?php 
+		
+	include '../conexion.php';
+	if(isset($_POST['devca'])){
+		$mysqli->query("update orden set ppto_dev='".$_POST['devca']."' where ord_id=".$_POST['idDevengado']);
+	}
+	if(isset($_POST['devpo'])){
+		$mysqli->query("update orden set poliza_dev='".$_POST['devpo']."' where ord_id=".$_POST['idDevengado']);
+	}
+	header("Location: ../projects.php");
+ ?>
